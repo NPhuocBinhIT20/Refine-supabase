99db889517ad4e5507b9ea548d6ab07e08b30f7b
unknown <binhnguyen110120@gmail.com>
2023-06-13 14:24:34 +0700

    second commit

--- example-app/src/interfaces/index.d.ts
+++ example-app/src/interfaces/index.d.ts
@@ -20,3 +20,42 @@ export interface IPost {
     categoryId: string;
     images: IFile[];
 }
+
+
+export interface ILabel {
+    id: string;
+    title: string;
+    color: string;
+}
+
+export interface IPriority {
+    id: string;
+    title: string;
+}
+
+export interface IStatus {
+    id: string;
+    title: string;
+}
+
+export interface ITask {
+    id: string;
+    title: string;
+    description: string;
+    start_time: string;
+    end_time: string;
+    label: string;
+    priority: string;
+    status: string;
+    users: string;
+}
+
+export interface ITaskFilterVariables {
+    title: string;
+    label: string;
+    priority: string;
+    users: string;
+    status: string;
+    start_time: [Dayjs, Dayjs];
+    end_time: [Dayjs, Dayjs];
+}
